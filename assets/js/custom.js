31b9f61f7e91c6b13f97cc5ebe54c94557995c25
Onup Chandra Barmon <anupme01@gmail.com>
2022-03-02 15:09:56 +0600

    last update gallay popup

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,4 @@
 jQuery(document).ready(function(){ 
-
 	// Home owlCarousel
 	jQuery('.hero_slider_area').owlCarousel({
 		stagePadding: 0,
@@ -10,23 +9,57 @@ jQuery(document).ready(function(){
 		dots: true,
 		items: 1,
 	});
+	// Popup JS 
+	jQuery('.zoom-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		closeOnContentClick: false,
+		closeBtnInside: false,
+		mainClass: 'mfp-with-zoom mfp-img-mobile',
+		image: {
+			verticalFit: true,
+			titleSrc: function (item) {
+				return item.el.attr('title') + ' &middot; <a class="image-source-link" href="' + item.el.attr('data-source') + '" target="_blank">image source</a>';
+			}
+		},
+		gallery: {
+			enabled: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300, // don't foget to change the duration also in CSS
+			opener: function (element) {
+				return element.find('img');
+			}
+		}
 
-	
-   //Dropdown menu 
-   jQuery('.country_dropdown').click(function () {
-	jQuery('.country_dropdown span').toggleClass('active_arrow');
-		return false;
 	});
-	jQuery('.country_dropdown ul li').click(function () {
-		jQuery('.country_dropdown span').removeClass('active_arrow');
-		return false;
-			
+	// Popup JS 
+	jQuery('.zoom-gallery-2').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		closeOnContentClick: false,
+		closeBtnInside: false,
+		mainClass: 'mfp-with-zoom mfp-img-mobile',
+		image: {
+			verticalFit: true,
+			titleSrc: function (item) {
+				return item.el.attr('title') + ' &middot; <a class="image-source-link" href="' + item.el.attr('data-source') + '" target="_blank">image source</a>';
+			}
+		},
+		gallery: {
+			enabled: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300, // don't foget to change the duration also in CSS
+			opener: function (element) {
+				return element.find('img');
+			}
+		}
+
 	});
 
-	
 
-		
-		
-	
 
 });
